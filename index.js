62a816b29703fd90c1a008e28c74cb657fa66254
Hakumai <inside.rice164@gmail.com>
2018-12-27 23:31:29 +0900

    Implement div components for background image as React components

--- index.js
+++ index.js
@@ -54,30 +54,57 @@ exports.decorateConfig = (config) => {
 
 exports.decorateTerms = (Terms, {React, notify, Notification}) => {
 
+  class WallPaperComponent extends React.Component {
+
+    constructor(props, context) {
+      super(props, context)
+    }
+
+    render() {
+      return React.createElement('div', {
+        className: 'profile',
+        style: {
+          backgroundImage: this.props.wallpaper ? `url(file://${this.props.filePath}` : null,
+          backgroundSize: this.props.wallpaper ? this.props.size : null,
+          color: !this.props.wallpaper ? this.props.color : null,
+          display: this.props.isActive ? 'block' : 'none'
+        },
+      })
+    }
+  }
+
   return class extends React.Component {
 
     constructor(props, context) {
       super(props, context);
+      this.componentDidMount = this.componentDidMount.bind(this);
 
       this.state = {
-        profileComponents: props.wallpaperConfig.map((config) => {
-          return React.createElement('div', {
-            className: 'profile',
-            style: {
-              backgroundImage: config.wallpaper ? `url(file://${config.filePath}` : null,
-              backgroundSize: config.wallpaper ? config.size : null,
-              color: !config.wallpaper ? config.color : null
-            }
-          })
-        })
+        isActiveProfile: props.wallpaperConfig.map(() => { return false })
       };
-      this._customChildrenBefore = React.createElement('div', {className: 'hyper-wallpaper-wrapper'}, ...this.state.profileComponents);
+    }
+
+    componentDidMount() {
+      let isActiveProfileCopy = this.state.isActiveProfile.slice();
+      isActiveProfileCopy[0] = true;
+      this.setState({
+        isActiveProfile: isActiveProfileCopy
+      })
     }
 
     render () {
       return React.createElement(Terms, Object.assign({}, this.props, {
-        customChildrenBefore: this._customChildrenBefore
-
+        customChildrenBefore: React.createElement('div', {className: 'hyper-wallpaper-wrapper'},
+          ...this.props.wallpaperConfig.map((config, index) => {
+            return React.createElement(WallPaperComponent, {
+              wallpaper: config.wallpaper,
+              filePath: config.filePath,
+              size: config.size,
+              color: config.size,
+              isActive: this.state.isActiveProfile[index]
+            });
+          })
+        )
       }));
     }
   };
